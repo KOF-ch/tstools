9928402b624dc1daad98b3756aa761180efbc260
Matthias Bannert <matthias.bannert@gmail.com>
2016-07-19 23:06:12 +0200

    added new function to fillup periods with NAs, useful for plotting

--- R/fillUpYearWithNAs.R
+++ R/fillUpYearWithNAs.R
@@ -3,7 +3,7 @@
 #' When plotting a time series you might want set the range of the plot a little wider than just the start and end date of the original series. This function add fills up the current period (typically year) with NA.
 #' 
 #' @param ts object of class ts
-
+#' @export
 fillUpYearWithNAs <- function(x){
   frq <- frequency(x)
   y <- floor(round(max(time(x)),digits=5))
