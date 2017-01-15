15858ad8adfa1056103e061884e813d1cc57a59b
Matthias Bannert <matthias.bannert@gmail.com>
2017-01-15 10:20:16 +0100

    modular interface for line plots

--- R/rangeUtils.R
+++ R/rangeUtils.R
@@ -1,15 +1,22 @@
-.getValueRange <- function(li){
-  value_range <- range(unlist(li),na.rm=T)
-  value_range <- trunc(value_range)
-  value_range <- c((floor(value_range[1]/10)-1)*10,
-                   (ceiling(value_range[2]/10)+1)*10)
-  value_range
+.getValueRange <- function(li,manual_value_range = NULL){
+  if(is.null(manual_value_range)){
+    value_range <- range(unlist(li),na.rm=T)
+    value_range <- trunc(value_range)
+    value_range <- c((floor(value_range[1]/10)-1)*10,
+                     (ceiling(value_range[2]/10)+1)*10)
+    value_range  
+  } else {
+    manual_value_range
+  }
+  
 }
 
-.getDateRange <- function(li){
-  value_range <- range(unlist(li),na.rm=T)
-  value_range <- trunc(value_range)
-  value_range <- c((floor(value_range[1]/10)-1)*10,
-                   (ceiling(value_range[2]/10)+1)*10)
-  value_range
+.getDateRange <- function(li, manual_date_range = NULL){
+  if(is.null(manual_date_range)){
+    ts_time <- unique(unlist(lapply(li,time)))
+    ts_time <- round(ts_time,digits = 5)
+    date_range <- range(ts_time)
+  } else{
+    manual_date_range
+  }
 }
---
+++ inst/notes.R
@@ -0,0 +1,156 @@
+# Flow: 
+# put two plots above each other... 
+# otherwise axes can't have different scales, i.e. common 
+# horizontal gridlines.. 
+
+# idea... 
+
+# 1. get background done, including x-axis, y-axis (left)
+# 2. plot line chart that relates to left axis
+
+
+plot(NULL,
+     xlim = c(2010,2015),
+     ylim = c(50,130),
+     axes = F
+     )
+box()
+
+par(new=T)
+
+tsplot(KOF$kofbarometer)
+
+par(new=T)
+
+plot(NULL,
+     xlim = c(2010,2015),
+     ylim = range(KOF$reference),
+     axes = F,
+     yaxs = "i",
+     yaxs = "i",
+     xlab = "",
+     ylab = ""
+)
+
+blankPlot <- function(s){
+  plot(NULL,
+       xlim = c(2010,2015),
+       ylim = range(s),
+       axes = F,
+       yaxs = "i",
+       yaxs = "i",
+       xlab = "",
+       ylab = ""
+  )
+}
+
+blankPlot(KOF$kofbarometer)
+box()
+lines(KOF$kofbarometer,col="blue")
+axis(2,xaxs="i")
+axis(1,yaxs="i")
+par(new=T)
+blankPlot(KOF$reference)
+par(new=T)
+barplot(t(KOF$reference))
+lines(KOF$reference,col="red",lty=2)
+axis(4)
+
+
+plot(KOF$kofbarometer,
+     xaxs="i",
+     yaxs="i")
+
+
+lines(KOF$reference,col="blue")
+par(new=T)
+# nice par new hält nur einmal !! crucial
+
+tsLinePlot <- function(tsl,
+                manual_value_range = NULL,
+                manual_date_range = NULL,
+                theme = NULL,
+                blank = F){
+  # move the ts to list to the higher level later on:
+  if(!is.list(tsl)) tsl <- as.list(tsl)
+  
+  if(is.null(theme)) theme <- initDefaultTsTheme()
+  # determine xlim, ylim
+  d <- list()
+  d$date_range <- .getDateRange(tsl,manual_date_range)
+  d$value_range <- .getValueRange(tsl,manual_value_range)
+  # empty plot with xlim and ylim
+  plot(NULL,
+       xlim = d$date_range,
+       ylim = d$value_range,
+       axes = F,
+       xlab = "",
+       ylab = ""
+  )
+  # add lines
+  for (i in 1:length(tsl)){
+    lines(tsl[[i]],
+          col = theme$line_colors[[i]],
+          lwd = ifelse(length(theme$lwd) > 1,
+                       theme$lwd[i],
+                       theme$lwd),
+          lty = ifelse(length(theme$lty) > 1,
+                       theme$lty[i],
+                       theme$lty)
+    )
+  }
+  
+  
+  d
+}
+
+addYAxis <- function(d,
+                     right = F,
+                     y_grd_steps){
+  stps <- abs(d$value_range[1]-
+                d$value_range[2])/y_grd_steps
+  tick_positions <- seq(d$value_range[1],
+                        d$value_range[2],
+                        by = ceiling(stps))  
+  if(right){
+    axis(4, at = tick_positions)
+  } else{
+    axis(2, at = tick_positions)
+  }
+  tick_positions
+}
+
+addXAxis <- function(d, steps){
+  axis(1, at = seq(floor(d$date_range[1]),
+                  ceiling(d$date_range[2]),
+                  by = steps))
+}
+
+addYGrids <- function(tick_positions,theme){
+  
+}
+
+
+xx <- tsLinePlot(KOF$kofbarometer)
+axis(1,at = seq(xx$date_range[1],
+                ceiling(xx$date_range[2]),by=1))
+addYAxis(xx,y_grd_steps = 4)
+box()
+par(new = T)
+yy <- tsLinePlot(KOF$reference)
+addYAxis(yy,right=T,y_grd_steps = 4)
+
+
+
+tsp2y <- function(l_tsl,r_tsl,
+                  l_manual_value_range = NULL,
+                  r_manual_value_range = NULL,
+                  manual_date_range = NULL,
+                  theme = NULL){
+  # 
+  
+  
+}
+
+.getValueRange(KOF$reference)
+.getDateRange()
