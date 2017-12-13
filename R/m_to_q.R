2d93358a13fb6dacf92ca399f96bebf7273d7c63
mbannert <matthias.bannert@gmail.com>
2017-12-13 16:20:04 +0100

    handle quarterly and monthly frequencies with NAs better

---
+++ R/m_to_q.R
@@ -0,0 +1,16 @@
+#' Turn monthly series with regular NAs to quarter
+#' 
+#' Monthly series with NAs in non-quarter months are turned to quarterly series.
+#' Series without NAs are just returned. 
+#' 
+#' @param series an object of class ts with monthly frequency
+#' 
+#' @export
+m_to_q <- function(series){
+  stopifinot(frequency(series) == 12)
+  if(!any(is.na(series))) return(series)
+  period <- start(series)[2]
+  q_start <- c(start(series)[1],rep(1:4,each=3)[period])
+  no_nas <- na.omit(as.numeric(series))
+  ts(no_nas, start = q_start, freq = 4)
+}
---
+++ R/tsqm.R
@@ -0,0 +1,28 @@
+#' Interpolate quarterly time series into monthly
+#' 
+#' Repeat quarterly variables two times to generate a monthly variable.
+#' 
+#' @param qts quarterly time series
+#' @examples 
+#' tsq <- ts(1:20,start=c(1990,1),frequency = 4)
+#' tsqm(tsq)
+#' @export
+tsqm <- function(qts){
+  if(frequency(qts) == 12) return(qts)
+  start_date <- start(qts)
+  mts <- as.vector(t(qts %*% t(c(1,1,1))))
+  mts <- ts(mts, start = .quarter_to_month(start_date), freq = 12)
+  mts
+  
+}
+
+.quarter_to_month <- function(sdate){
+  y <- sdate[1]
+  qms <- c(1,4,7,10)
+  out <- qms[sdate[2]]
+  if(is.na(out)) return(c(y,1))
+  c(y,out)
+}
+
+
+
