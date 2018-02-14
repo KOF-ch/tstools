c7dd08becd3c390a3a787aca96a9bcbe0c79fad1
thoenis <sthoeni@kof.ethz.ch>
2018-02-14 09:36:53 +0100

    reconvert date_zoo to yearmon/yearqtr for irregular tsi
    date_zoo is cast as.numeric to be able to store monthly and
    quarterly dates in the same column. however, the constructor for
    xts requires order.by be in a "time-based" format.

--- R/import_helpers.R
+++ R/import_helpers.R
@@ -16,13 +16,17 @@ json_to_ts <- function(json_data) {
 long_to_ts <- function(data) {
   data_dt <- as.data.table(data)
   
-  data_dt[, date_zoo := as.numeric(as.yearmon(date)), by = series]
-  data_dt[is.na(date_zoo), date_zoo := as.numeric(as.yearqtr(date))]
+  data_dt[, `:=`(date_zoo = as.numeric(as.yearmon(date)), frq = 12), by = series]
+  data_dt[is.na(date_zoo), `:=`(date_zoo = as.numeric(as.yearqtr(date)), frq = 4)]
   
   dt_of_lists <- data_dt[, {
     dT <- diff(date_zoo)
     if(any(diff(dT) > 1e-6)) {
-      list(ts_object = list(xts(value, order.by = as.yearmon(date_zoo))))
+      if(frq[1] == 4) {
+        list(ts_object = list(xts(value, order.by = as.yearqtr(date_zoo))))
+      } else {
+        list(ts_object = list(xts(value, order.by = as.yearmon(date_zoo))))
+      }
     } else {
       list(ts_object = list(ts(value, start = .SD[1, date_zoo], end = .SD[.N, date_zoo], deltat = dT[1])))
     }
