895aee95ed8d062e2fe32bda485298eb77a5b1a8
mbannert <matthias.bannert@gmail.com>
2018-02-14 16:25:27 +0100

    added colorblind palette and new examples

---
+++ R/CHGDP.R
@@ -0,0 +1,18 @@
+#' CH GDP Growth Contributions
+#'
+#' A list of time series containing sector contributions to Swiss GDP over time. 
+#' 
+#'
+#' @format List list of six time series of class ts, 
+#' containing contributions to Swiss GDP growth
+#' \describe{
+#'   \item{manufacturing}{Growth contribution of manufacturing.}
+#'   \item{energy}{Growth contribution of energy, water sector}
+#'   \item{construction}{Growth contribution construction sector.}
+#'   \item{hotels}{Growth contribution of hotels.}
+#'   \item{fin_insur}{Growth contribution of financial services and insurances.}
+#'   \item{other}{Growth contribution of other sectors.}
+#' }
+#' @source \url{https://www.seco.admin.ch/seco/en/home/wirtschaftslage---wirtschaftspolitik/Wirtschaftslage/bip-quartalsschaetzungen-/daten.html}
+#' 
+"CHGDP"
---
+++ R/color_blind.R
@@ -0,0 +1,10 @@
+#' Provide Colorblind Compliant Colors 
+#' 
+#' 8 Hex RGB color defintions suitable for charts for colorblind people.
+#' 
+#' @export
+color_blind <- function(){
+  c("#000000", "#E69F00", "#56B4E9",
+    "#009E73", "#F0E442", "#0072B2",
+    "#D55E00", "#CC79A7")
+} 
