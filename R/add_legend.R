685491b93488e535a0b4fdc7b5ab3ed04d1810ae
thoenis <sthoeni@kof.ethz.ch>
2018-03-06 13:50:38 +0100

    Remove extra plot layer "hack" in add_legend
    See #89

--- R/add_legend.R
+++ R/add_legend.R
@@ -4,32 +4,31 @@ add_legend <- function(tsln,
                       tsrn = NULL,
                       left_as_bar = F,
                       theme = init_tsplot_theme()){
-  par(fig = c(0, 1, 0, 1),
-      oma = c(0.2, 3, 2, 1),
-      mar = c(0, 0, 0, 0),
-      new = TRUE)
-  plot(0, 0, type="n", bty="n", xaxt="n", yaxt="n")
-  
   ll <- length(tsln)
+  
+  # Pop quiz: Why are the legends placed relative to the top? Because then their anchor is at the top
+  # and they grow downwards instead of up into the plotting area.
   
   if(is.null(tsrn)){
     if(!left_as_bar){
-      legend("bottomleft", 
+      legend("topleft", 
              legend = tsln,
              ncol = theme$legend_col,
-             #horiz = TRUE, 
              bty = "n",
+             xpd = NA,
+             inset = c(0, 1.2),
              col = na.omit(theme$line_colors[1:ll]),
              lty = na.omit(theme$lty[1:ll]),
              lwd = na.omit(theme$lwd[1:ll]),
              x.intersp = theme$legend_intersp_x,
              y.intersp = theme$legend_intersp_y)    
     } else {
-      legend("bottomleft", 
+      legend("topleft", 
              legend = tsln,
              ncol = theme$legend_col,
-             #horiz = TRUE, 
              bty = "n",
+             xpd = NA,
+             inset = c(0, 1.2),
              fill = na.omit(theme$bar_fill_color[1:ll]),
              x.intersp = theme$legend_intersp_x,
              y.intersp = theme$legend_intersp_y)  
@@ -47,37 +46,45 @@ add_legend <- function(tsln,
     lwd <- rep(theme$lwd, ceiling(lb/length(theme$lwd)))
     
     if(!left_as_bar){
-      legend("bottomleft", 
+      legend("topleft", 
              legend = tsln,
              ncol = theme$legend_col,
              bty = "n",
+             xpd = NA,
+             inset = c(0, 1.2),
              col = na.omit(line_colors[1:ll]),
              lty = na.omit(lty[1:ll]),
              lwd = na.omit(lwd[1:ll]),
              x.intersp = theme$legend_intersp_x,
              y.intersp = theme$legend_intersp_y)
-      legend("bottomright", 
+      legend("topright", 
              legend = tsrn,
              ncol = theme$legend_col,
              bty = "n",
+             xpd = NA,
+             inset = c(0, 1.2),
              col = na.omit(line_colors[(ll+1):lb]),
              lty = na.omit(lty[(ll+1):lb]),
              lwd = na.omit(lwd[(ll+1):lb]),
              x.intersp = theme$legend_intersp_x,
              y.intersp = theme$legend_intersp_y)
     } else {
-      legend("bottomleft",
+      legend("topleft",
              legend = tsln,
              ncol = theme$legend_col,
              bty = "n",
              border = NA,
+             xpd = NA,
+             inset = c(0, 1.2),
              fill = bar_fill_colors[1:ll],
              x.intersp = theme$legend_intersp_x,
              y.intersp = theme$legend_intersp_y)
-      legend("bottomright",
+      legend("topright",
              legend = tsrn,
              bty = "n",
              border = NA,
+             xpd = NA,
+             inset = c(0, 1.2),
              ncol = theme$legend_col,
              lty = lty[1:lr],
              lwd = lwd[1:lr],
