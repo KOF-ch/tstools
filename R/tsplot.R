d1ad5ad54259ed91e79c50e14b302eabfcbeeacc
thoenis <sthoeni@kof.ethz.ch>
2018-03-06 13:51:13 +0100

    Move title stuff to its own function

--- R/tsplot.R
+++ R/tsplot.R
@@ -441,7 +441,7 @@ tsplot.list <- function(...,
     tt_r <- theme
     tt_r$line_colors <- tt_r$line_colors[start_r]
     if(!all(is.na(tt_r$lwd[start_r]))) tt_r$lwd <- na.omit(tt_r$lwd[start_r])
-    if(!all(is.na(tt_r$lwd[start_r]))) tt_r$lty <- na.omit(tt_r$lty[start_r])
+    if(!all(is.na(tt_r$lty[start_r]))) tt_r$lty <- na.omit(tt_r$lty[start_r])
     
     draw_ts_lines(tsr,theme = tt_r)
     
@@ -467,40 +467,8 @@ tsplot.list <- function(...,
   }
   
   # add title and subtitle
-  if(!is.null(plot_title)){
-    if(!any(is.na(theme$title_transform))){
-      plot_title <- do.call(theme$title_transform,
-                            list(plot_title))
-    } 
-    title(main = plot_title, adj = theme$title_adj,
-          line = theme$title_line,
-          outer = theme$title_outer,
-          cex.main = theme$title_cex.main)    
-  }
+  add_title(plot_title, plot_subtitle, plot_subtitle_r, theme)
   
-  if(!is.null(plot_subtitle)){
-    if(!is.null(theme$subtitle_transform)){
-      plot_subtitle <- do.call(theme$subtitle_transform,
-                               list(plot_subtitle))
-    } 
-    mtext(plot_subtitle, adj = theme$title_adj,
-          line = theme$subtitle_line,
-          outer = theme$subtitle_outer,
-          cex = theme$subtitle_cex)    
-  }
-  
-  
-  if(!is.null(plot_subtitle_r)){
-    if(!is.null(theme$subtitle_transform)){
-      plot_subtitle_r <- do.call(theme$subtitle_transform,
-                                 list(plot_subtitle_r))
-    } 
-    mtext(plot_subtitle_r,
-          adj = theme$subtitle_adj_r,
-          line = theme$subtitle_line,
-          outer = theme$subtitle_outer,
-          cex = theme$subtitle_cex)    
-  }
   # return axes and tick info, as well as theme maybe? 
   if(!quiet){
     output <- list(left_range = tsl_r,
