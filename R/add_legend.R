11da189b148a8912d010154e53c1219afb4bea68
thoenis <sthoeni@kof.ethz.ch>
2018-04-11 15:02:36 +0200

    Slightly nudge legends up

--- R/add_legend.R
+++ R/add_legend.R
@@ -8,7 +8,7 @@ add_legend <- function(tsln,
   
   plot_size_in_in <- dev.size()*(par("plt")[c(2, 4)] - par("plt")[c(1, 3)])
   
-  inset_y <- 1 + theme$legend_margin_top/plot_size_in_in[2]
+  inset_y <- 0.98 + theme$legend_margin_top/plot_size_in_in[2]
   
   # Pop quiz: Why are the legends placed relative to the top? Because then their anchor is at the top
   # and they grow downwards instead of up into the plotting area.
