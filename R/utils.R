a468fea3af13184b14037e64f12937f19dd40470
thoenis <sthoeni@kof.ethz.ch>
2018-05-02 15:03:47 +0200

    Make findTicks respect range_must_not_cross_zero
    The bit nudging the time series towards the middle if there is an empty
    tick at the top/bottom of the plot, did not take the boundary opposite
    the "movement direction" into account.
    
    Fixes #154

--- R/utils.R
+++ R/utils.R
@@ -48,7 +48,7 @@ getGlobalXInfo <- function(tsl,tsr,fill_up_start){
                                           floor(global_x$monthly_tick_pos) == 0.5,
                                         as.character(floor(global_x$monthly_tick_pos)),
                                         NA)
-                                        
+  
   global_x
 }
 
@@ -69,7 +69,7 @@ sanitizeTsr <- function(tsr){
     stop("Time series object to be plotted on the right axis, 
          has to be either of class ts, mts or list.")
   }
-}
+  }
 
 #' @importFrom graphics abline
 addYGrids <- function(tick_positions,theme){
@@ -104,14 +104,14 @@ findTicks <- function(r, tick_count, preferred_gap_sizes, preserve_sign = FALSE)
   
   # nudge the generated range around a bit to ensure the series are more or less "centered"
   # i.e. there are no empty ticks
-  lb_too_low <- r[1] > lb + gaps & (!preserve_sign | sign(lb) == sign(lb + gaps/2))
+  lb_too_low <- r[1] > lb + gaps & (!preserve_sign | (sign(ub) == sign(ub + gaps/2) && sign(lb) == sign(lb + gaps/2)))
   lb[lb_too_low] <- lb[lb_too_low] + gaps[lb_too_low]/2
   ub[lb_too_low] <- ub[lb_too_low] + gaps[lb_too_low]/2
   
-  ub_too_high <- r[2] < ub - gaps & (!preserve_sign | sign(ub) == sign(ub - gaps/2))
+  ub_too_high <- r[2] < ub - gaps & (!preserve_sign | (sign(ub) == sign(ub - gaps/2) && sign(lb) == sign(lb - gaps/2)))
   lb[ub_too_high] <- lb[ub_too_high] - gaps[ub_too_high]/2
   ub[ub_too_high] <- ub[ub_too_high] - gaps[ub_too_high]/2
-
+  
   seqs <- list()
   for(i in seq_along(gaps)) {
     seqs[[i]] <- seq(lb[i],ub[i],gaps[i])
