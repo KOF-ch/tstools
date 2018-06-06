265c599fe33eaf5ebd7d0d801ff1f382e38d4505
thoenis <sthoeni@kof.ethz.ch>
2018-06-06 09:31:56 +0200

    Change legend_margin_top to % of device height
    Otherwise auto-bottom-margin calculation turns circular:
    legend margin is defined in % of plot height depends on by bottom margin
    depends on legend margin

--- R/add_legend.R
+++ R/add_legend.R
@@ -6,9 +6,10 @@ add_legend <- function(tsln,
                       theme = init_tsplot_theme()){
   ll <- length(tsln)
   
-  # plot_size_in_in <- dev.size()*(par("plt")[c(2, 4)] - par("plt")[c(1, 3)])
+  plot_size_in_in <- dev.size()*(par("plt")[c(2, 4)] - par("plt")[c(1, 3)])
+  dev_size <- dev.size()
   
-  inset_y <- 0.98 + theme$legend_margin_top/100 #/plot_size_in_in[2]
+  inset_y <- 0.98 + (plot_size_in_in[1]*theme$legend_margin_top)/(100*dev_size[1]) #/plot_size_in_in[2]
   
   # Pop quiz: Why are the legends placed relative to the top? Because then their anchor is at the top
   # and they grow downwards instead of up into the plotting area.
