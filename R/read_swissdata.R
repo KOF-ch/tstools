7295da68c83a26dad2db500d9d0d05e083d59a8f
mbannert <matthias.bannert@gmail.com>
2018-06-19 17:59:36 +0200

    messing around with data.table, at least got a regexp going worth of commit

---
+++ R/read_swissdata.R
@@ -0,0 +1,12 @@
+read_swissdata <- function(path, key_columns) {
+  path <- "~/repositories/github/swissdata/wd/ch.bfs.dhu/ch.bfs.dhu.csv"
+  dataset <- gsub("(.+wd\\/)(.+)(\\/.+)","\\2",path)
+  raw <- data.table::fread(path)
+  key_columns <- c("structure","trans","price.adj","seas")
+  
+#  raw[,ts_key := paste(deparse(substitute(key_coumns)),sep=".")]
+}
+
+
+
+
