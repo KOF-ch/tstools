42420af9a1cc6900bd3c1feda4e1525d4799d4e3
thoenis <sthoeni@kof.ethz.ch>
2018-07-10 11:07:48 +0200

    Fix colors, lwd, lty of right legend
    Another misuse of ifelse

--- R/add_legend.R
+++ R/add_legend.R
@@ -104,11 +104,11 @@ add_legend <- function(tsln,
     pch_r <- rep(NA, n_tot_r)
     pch_r[is_ci_r] <- 15
     col_r <- rep(NA, n_tot_r)
-    col_r[!is_ci_r] <- theme$line_colors[ifelse(left_as_bar, 1:lr, (ll+1):lb)]
+    col_r[!is_ci_r] <- theme$line_colors[`if`(left_as_bar, 1:lr, (ll+1):lb)]
     
     ci_color_indices_r <- cumsum(!is_ci_r)[is_ci_r]
     ci_legend_colors_r <- c()
-    right_ci_colors <- theme$ci_colors[ifelse(left_as_bar, 1:lr, (ll+1):lb)]
+    right_ci_colors <- theme$ci_colors[`if`(left_as_bar, 1:lr, (ll+1):lb)]
     for(i in unique(ci_color_indices_r)) {
       ci_legend_colors_r <- c(
         ci_legend_colors_r, 
@@ -118,9 +118,9 @@ add_legend <- function(tsln,
     
     col_r[is_ci_l] <- ci_legend_colors_r #namedColor2Hex(theme$ci_colors[ifelse(left_as_bar, 1:lr, (ll+1):lb)], theme$ci_alpha)[cumsum(!is_ci_r)[is_ci_r]]
     lty_r <- rep(0, n_tot_r)
-    lty_r[!is_ci_r] <- theme$lty[ifelse(left_as_bar, 1:lr, (ll+1):lb)]
+    lty_r[!is_ci_r] <- theme$lty[`if`(left_as_bar, 1:lr, (ll+1):lb)]
     lwd_r <- rep(0, n_tot_r)
-    lwd_r[!is_ci_r] <- theme$lwd[ifelse(left_as_bar, 1:lr, (ll+1):lb)]
+    lwd_r[!is_ci_r] <- theme$lwd[`if`(left_as_bar, 1:lr, (ll+1):lb)]
     
     legend("topright", 
            legend = legend_r,
